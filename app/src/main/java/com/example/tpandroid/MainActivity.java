05a045e21227ffd4cc130a51162764486ed6d68d
PazMatias <matiasp975@gmail.com>
2021-10-16 16:53:30 -0300

    Se implemento el patron de bloqueo

--- app/src/main/java/com/example/tpandroid/MainActivity.java
+++ app/src/main/java/com/example/tpandroid/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.tpandroid;
 
+import static com.example.tpandroid.R.id.pattern_lock_view;
+
 import android.os.Bundle;
 
+import com.andrognito.patternlockview.PatternLockView;
+import com.andrognito.patternlockview.listener.PatternLockViewListener;
+import com.andrognito.patternlockview.utils.PatternLockUtils;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
@@ -9,12 +14,16 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.tpandroid.ui.main.SectionsPagerAdapter;
 import com.example.tpandroid.databinding.ActivityMainBinding;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,19 +36,50 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = binding.viewPager;
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = binding.tabs;
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = binding.fab;
-
-        fab.setOnClickListener(new View.OnClickListener() {
+//        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+//        ViewPager viewPager = binding.viewPager;
+//        viewPager.setAdapter(sectionsPagerAdapter);
+//        TabLayout tabs = binding.tabs;
+//        tabs.setupWithViewPager(viewPager);
+//        FloatingActionButton fab = binding.fab;
+//
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        PatternLockView patternLockView = findViewById(pattern_lock_view);
+        patternLockView.addPatternLockListener(new PatternLockViewListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onStarted() {
+
+            }
+
+            @Override
+            public void onProgress(List progressPattern) {
+
+            }
+
+            @Override
+            public void onComplete(List pattern) {
+                Log.d(getClass().getName(), "Pattern complete: " +
+                        PatternLockUtils.patternToString(patternLockView, pattern));
+                if (PatternLockUtils.patternToString(patternLockView, pattern).equalsIgnoreCase("123")) {
+                    patternLockView.setViewMode(PatternLockView.PatternViewMode.CORRECT);
+                    Toast.makeText(MainActivity.this, "Bien pelotudo", Toast.LENGTH_LONG).show();
+                } else {
+                    patternLockView.setViewMode(PatternLockView.PatternViewMode.WRONG);
+                    Toast.makeText(MainActivity.this, "Incorrect password", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCleared() {
+
             }
         });
+
     }
 }

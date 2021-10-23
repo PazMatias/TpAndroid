7737e0ac6f7533e2c2436ef37e7510f7f212df18
Rodrigo Franco <rodrigo.franco@codetria.com>
2021-10-23 20:50:36 -0300

    Se agregan Headers a endpoints a utilizar en SoadService

--- app/src/main/java/com/example/tpandroid/services/SoaService.java
+++ app/src/main/java/com/example/tpandroid/services/SoaService.java
@@ -10,19 +10,23 @@ import com.example.tpandroid.retrofit.requests.RegisterRequest;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 
 public interface SoaService {
 
-
-
+    @Headers({"content-type: application/json"})
     @POST("api/register")
     Call<RegisterResponse> register(@Body RegisterRequest request);
+    @Headers({"content-type: application/json"})
     @POST("api/login")
-    Call<LoginResponse> login(@Body LoginRequest request);
+    Call<LoginResponse> login(@Header("Authorization") String authToken, @Body LoginRequest request);
+    @Headers({"content-type: application/json"})
     @PUT("api/refresh")
-    Call<RefreshTokenResponse> refreshToken();
+    Call<RefreshTokenResponse> refreshToken(@Header("Authorization") String authToken);
+    @Headers({"content-type: application/json"})
     @POST("api/event")
-    Call<RegisterEventResponse> registerEvents(@Body RegisterEventRequest request);
+    Call<RegisterEventResponse> registerEvents(@Header("Authorization") String authToken,@Body RegisterEventRequest request);
 }
